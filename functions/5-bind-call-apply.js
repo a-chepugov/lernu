ca0bc75fe38dc742893870962af5b53bb78aed43
Алексей Чепугов <a.chepugov@gmail.com>
2021-04-03 23:16:47 +0300

    chore (functions) output formatting

--- functions/5-bind-call-apply.js
+++ functions/5-bind-call-apply.js
@@ -3,11 +3,26 @@ function mul(a, b) {
 }
 
 const mul4 = mul.bind(null, 4);
-console.log("\nconst mul4 = mul.bind(null, 4);\nmul4(5) ===", mul4(5));
+console.log(
+  "\nconst mul4 = mul.bind(null, 4);\nmul4(5) ===",
+  mul4(5));
 
-console.log("\nmul(2, 3) ===", mul(2, 3));
-console.log("\nmul.call(null, 2, 3) ===", mul.call(null, 2, 3));
+
+console.log(
+  "\nmul(2, 3) ===",
+  mul(2, 3));
+
+console.log(
+  "\nmul.call(null, 2, 3) ===",
+  mul.call(null, 2, 3));
+
 
 const params = [2, 3];
-console.log("\nmul(...params)", mul(...params));
-console.log("\nmul.apply(null, params)", mul.apply(null, params));
+
+console.log(
+  "\nmul(...params)",
+  mul(...params));
+
+console.log(
+  "\nmul.apply(null, params)",
+  mul.apply(null, params));
